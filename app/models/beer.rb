bc95230e54f8c1b3d39d04d4ff0653791e6370c2
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-01-26 09:11:00 +0200

    simplified average_rating method

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,8 +3,6 @@ class Beer < ActiveRecord::Base
   has_many :ratings
 
   def average_rating
-    sum = 0
-    self.ratings.each { |rating| sum += rating.score}
-    average = sum / (self.ratings.count == 0 ? 1 : self.ratings.count)
+    self.ratings.count == 0 ? 0 : self.ratings.average(:score).round(2)
   end
 end
