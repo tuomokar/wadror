bf12ac5b1095f8723674762796739ee6409cc727
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-01-26 08:06:42 +0200

    added method for average rating

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,10 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = 0
+    self.ratings.each { |rating| sum += rating.score}
+    average = sum / (self.ratings.count == 0 ? 1 : self.ratings.count)
+  end
 end
