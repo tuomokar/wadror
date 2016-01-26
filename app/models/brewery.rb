c23bb5563b8222a50231b260c615627daa865cc0
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-01-26 22:28:42 +0200

    dependency destroying + average_rating method

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,7 +1,12 @@
 class Brewery < ActiveRecord::Base
-  has_many :beers
+  has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
 
   def to_s
     self.name
+  end
+
+  def average_rating
+    self.ratings.count == 0 ? 0 : self.ratings.average(:score).round(2)
   end
 end
