a250f95529ca5ab51f05ca09ec657a5c075aab25
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-01 19:08:50 +0200

    refactored logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # defines method to be used visbile in views
+  helper_method :current_uesr
+
+  def current_user
+    return nil if session[:user_id].nil?
+    User.find(session[:user_id])
+  end
 end
