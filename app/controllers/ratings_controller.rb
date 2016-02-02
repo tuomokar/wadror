905a8d656076c250de3b11f78badafc704d425a4
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-02 12:36:56 +0200

    added validations

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,12 +9,15 @@ class RatingsController < ApplicationController
   end
 
   def create
-    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-    current_user.ratings << rating
+    @rating = Rating.create params.require(:rating).permit(:score, :beer_id)
 
-    session[:last_rating] = "#{rating.beer.name}, #{rating.score} points"
-
-    redirect_to ratings_path
+    if @rating.save
+      current_user.ratings << rating
+      redirect_to ratings_path
+    else
+      @beers = Beer.all
+      render :new
+    end
   end
 
   def destroy
