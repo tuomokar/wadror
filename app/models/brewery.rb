14f310926e36cc113d51a8f2fae06d644e81ae17
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-02 12:40:50 +0200

    added brewery validations

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,11 +2,22 @@ class Brewery < ActiveRecord::Base
   # module to count average ratings
   include RatingAverage
 
+  validate :not_in_future
+  validates :year, numericality: { greater_than_or_equal_to: 1042,
+                                    only_integer: true }
+  validates :name, presence: true
+
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
   def to_s
     self.name
+  end
+
+  def not_in_future
+    if year.present? && year > Time.now.year
+      errors.add(:year, "can't be in the future")
+    end
   end
 
 end
