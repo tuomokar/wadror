00a4a2257c46ddce5594ab1287292e705ed0ac00
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-02 12:41:07 +0200

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   include RatingAverage
 
+  validates :username, uniqueness: true,
+                      length: { minimum: 3, maximum: 15}
+
   has_many :ratings
 end
