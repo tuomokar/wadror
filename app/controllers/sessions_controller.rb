178ecf537793a589baaf4fd02e8423b925b7802a
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-03 18:12:31 +0200

    added authentication on signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by username: params[:username]
 
-    if user.nil?
-      redirect_to :back, notice: "User #{params[:username]} does not exist!"
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back!"
     else
+      redirect_to :back, notice: "User #{params[:username]} does not exist!"
       session[:user_id] = user.id
       redirect_to user, notice: "Welcome back!"
     end
