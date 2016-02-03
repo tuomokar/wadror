30b42beb28981b47e87dc44c710f9d572ce83f69
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-03 15:24:38 +0200

    added validation for beer club existing

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,12 @@
 class Membership < ActiveRecord::Base
   belongs_to :beer_club
   belongs_to :user
+
+  validate :beer_club_exists
+
+  def beer_club_exists
+    if (BeerClub.all.find_by_id beer_club_id).nil?
+      errors.add(:beer_club_id, "must exist")
+    end
+  end
 end
