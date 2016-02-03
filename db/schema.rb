c7f14ede052fcfd4c6d61ae4a549f855a75827fd
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-03 16:25:24 +0200

    password_digest added to usrs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202122443) do
+ActiveRecord::Schema.define(version: 20160203140843) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -52,9 +52,10 @@ ActiveRecord::Schema.define(version: 20160202122443) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   limit: 255
+    t.string   "username",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
