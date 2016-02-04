a78392353df5449184e22665dd3c478d60e60d00
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-04 15:46:13 +0200

    corrected rating to be tied to user on creation of rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -12,7 +12,7 @@ class RatingsController < ApplicationController
     @rating = Rating.create params.require(:rating).permit(:score, :beer_id)
 
     if @rating.save
-      current_user.ratings << rating
+      current_user.ratings << @rating
       redirect_to ratings_path
     else
       @beers = Beer.all
