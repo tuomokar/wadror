c24accd0e85d0ab2d8e5520739308d580c57b2bf
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-08 11:13:00 +0200

    only logged in users can rate beers

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -11,7 +11,9 @@ class RatingsController < ApplicationController
   def create
     @rating = Rating.create params.require(:rating).permit(:score, :beer_id)
 
-    if @rating.save
+    if current_user.nil?
+      redirect_to signin_path, notice:'you should be signed in'
+    elsif @rating.save
       current_user.ratings << @rating
       redirect_to ratings_path
     else
