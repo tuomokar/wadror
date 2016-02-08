2e2f2128c9deb769634402ba106d590c99ad7229
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-08 13:15:01 +0200

    rspec migration

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20160203140843) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",        limit: 255
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
