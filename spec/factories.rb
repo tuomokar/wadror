a2477daed6c8e2ce261273a1cc61164d506027b0
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-12 10:11:34 +0200

    factory

---
+++ spec/factories.rb
@@ -0,0 +1,71 @@
+FactoryGirl.define do
+  factory :user do
+    username "Pekka"
+    password "Foobar1"
+    password_confirmation "Foobar1"
+  end
+
+  factory :rating do
+    score 10
+  end
+
+  factory :rating2, class: Rating do
+    score 20
+  end
+
+  factory :brewery, class: Brewery do
+    name "anonymous"
+    year 1900
+  end
+
+  factory :brewery2, class: Brewery do
+    name "brewery2"
+    year 1900
+  end
+
+  factory :brewery3, class: Brewery do
+    name "brewery3"
+    year 1900
+  end
+
+  factory :brewery4, class: Brewery do
+    name "brewery4"
+    year 1900
+  end
+
+  factory :brewery5, class: Brewery do
+    name "brewery5"
+    year 1900
+  end
+
+  factory :beer do
+    name "anonymous"
+    brewery
+    style "Lager"
+  end
+
+  factory :beer2, class: Beer do
+    name "anonymous"
+    brewery
+    style "Weizen"
+  end
+
+  factory :beer3, class: Beer do
+    name "anonymous"
+    brewery
+    style "Pale ale"
+  end
+
+  factory :beer4, class: Beer do
+    name "anonymous"
+    brewery
+    style "IPA"
+  end
+
+  factory :beer5, class: Beer do
+    name "anonymous"
+    brewery
+    style "Porter"
+  end
+
+end
