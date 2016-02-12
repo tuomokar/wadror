5e4c6c35449de2841c75c2b9ea5d2a7a17c9af19
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-12 10:08:24 +0200

    username limit

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20160203140843) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string   "username",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
