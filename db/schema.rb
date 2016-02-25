73a33cc21cc5c33fe0ab65f73317d5d263d9fbe8
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-25 21:51:01 +0200

    activity for breweries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222211322) do
+ActiveRecord::Schema.define(version: 20160223182740) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160222211322) do
     t.integer  "year"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active"
   end
 
   create_table "memberships", force: :cascade do |t|
