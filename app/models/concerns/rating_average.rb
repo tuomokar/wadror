546cf76cac2c5983c28c21083013fe50540a2611
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-25 22:35:50 +0200

    rounds by 1 decimal

--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,6 +2,6 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
-    self.ratings.count == 0 ? 0 : self.ratings.average(:score).round(2)
+    self.ratings.count == 0 ? 0 : self.ratings.average(:score).round(1)
   end
 end
