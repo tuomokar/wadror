040294f328559e80f431b0bd137eb4ee7a4a4402
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-26 17:42:12 +0200

    ban status added to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160226090236) do
+ActiveRecord::Schema.define(version: 20160226123139) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160226090236) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.boolean  "admin"
+    t.boolean  "banned"
   end
 
 end
