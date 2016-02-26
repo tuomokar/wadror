912ea06876112950c8d0e1c74056ffc74f98c7cb
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-26 14:27:35 +0200

    only admins see remove buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     unless current_user.nil?
       edit = link_to('Edit', url_for([:edit, item]), class:"btn btn-primary")
       del = link_to('Destroy', item, method: :delete,
-                    data: {confirm: 'Are you sure?'}, class:"btn btn-danger")
+                    data: {confirm: 'Are you sure?'}, class:"btn btn-danger") if current_user.admin
       raw("#{edit} #{del}")
     end
   end
