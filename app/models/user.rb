2602cf5fa7cedf6c9a3b0215e00855739a3c734a
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-27 00:18:30 +0200

    refactored favorite methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,88 +20,28 @@ class User < ActiveRecord::Base
     ratings.order(score: :desc).limit(1).first.beer
   end
 
-  def favorite_style
-    return nil if beers.empty?
-    styles = Hash.new
-    ratings.each do |r|
-      if styles[r.beer.style].nil?
-        styles[r.beer.style] = Array.new(2)
-        set_initial_count_and_score(styles, r)
-      end
-      styles[r.beer.style][0] += r.score
-      styles[r.beer.style][1] += 1
-    end
-    averages = total_rating_averages(styles)
-    get_element_with_highest_rating(averages)
+  def favorite_brewery
+    favorite :brewery
   end
 
-  def favorite_brewery
-    return nil if beers.empty?
-    breweries = Hash.new
-    ratings.each do |r|
-      if breweries[r.brewery.name].nil?
-        breweries[r.brewery.name] = Array.new(2)
-        set_initial_count_and_score_for_brewery(breweries, r)
-      end
-      breweries[r.brewery.name][0] += r.score
-      breweries[r.brewery.name][1] += 1
-    end
-    averages = total_rating_averages(breweries)
-    name = get_element_with_highest_rating(averages)
-    find_brewery_by_brewery_name(name)
+  def favorite_style
+    favorite :style
+  end
+
+  def favorite(category)
+    return nil if ratings.empty?
+
+    rated = ratings.map{ |r| r.beer.send(category) }.uniq
+    rated.sort_by { |item| -rating
+  end
+
+  def rating_of(category, item)
+    ratings_of = ratings.select{ |r| r.beer.send(category)==item }
+    ratings_of.map(&:score).inject(&:+) / ratings_of.count.to_f
   end
 
   def self.top
     sorted_by_rating_in_desc_order = User.all.sort_by{ |u| -(u.ratings.count) }
     sorted_by_rating_in_desc_order.take(3)
   end
-
-  private
-
-    def find_brewery_by_brewery_name(name)
-      breweries.find { |b| b.name = name}
-    end
-
-    def get_element_with_highest_rating(averages)
-      averages.max_by{ |k, v| v}[0]
-    end
-
-    def total_rating_averages(hash)
-      averages = Hash.new
-      hash.each do |k, array|
-        averages[k] = count_rating_average(array)
-      end
-      averages
-    end
-
-    def count_rating_average(array)
-      array[0] / array[1]
-    end
-
-    def set_initial_count_and_score(styles, rating)
-      set_initial_count(styles, rating)
-      set_initial_score(styles, rating)
-    end
-
-    def set_initial_count(styles, rating)
-      styles[rating.beer.style][1] = 0
-    end
-
-    def set_initial_score(styles, rating)
-      styles[rating.beer.style][0] = 0
-    end
-
-    def set_initial_count_and_score_for_brewery(breweries, rating)
-      set_initial_count_for_brewery(breweries, rating)
-      set_initial_score_for_brewery(breweries, rating)
-    end
-
-    def set_initial_count_for_brewery(breweries, rating)
-      breweries[rating.beer.brewery.name][1] = 0
-    end
-
-    def set_initial_score_for_brewery(breweries, rating)
-      breweries[rating.beer.brewery.name][0] = 0
-    end
-
 end
