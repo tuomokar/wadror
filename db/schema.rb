4cc3ff82f694c67b84bfd7e4135ee307c01ac1ad
tuomokar <tuomo.a.karkkainen@helsinki.fi>
2016-02-26 13:52:06 +0200

    admin users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223182740) do
+ActiveRecord::Schema.define(version: 20160226090236) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20160223182740) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin"
   end
 
 end
